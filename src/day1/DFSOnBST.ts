71d834ee84466339268027f528d0a7e6c6ddc96c
urostripunovic <39649964+urostripunovic@users.noreply.github.com>
2024-01-17 14:01:43 +0100

    Trees + traversal/search

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,13 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function find(curr: BinaryNode<number> | null, needle: number): boolean {
+    if (!curr) return false;
 
+    if (curr.value === needle) return true;
+
+    if (curr.value < needle) return find(curr.right, needle);
+    
+    return find(curr.left, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return find(head, needle);
 }
